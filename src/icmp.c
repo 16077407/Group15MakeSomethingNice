b2475aeae0f1212cbd0e61019ac33abb12cff6f3
David Gall <d.c.c.m.gall@student.vu.nl>
2020-09-16 14:50:40 +0200

    Include Anna-Ruth code

---
+++ src/icmp.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright [2020] [Animesh Trivedi]
+ *
+ * This code is part of the Advanced Network Programming (ANP) course
+ * at VU Amsterdam.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+#include "icmp.h"
+#include "ip.h"
+#include "utilities.h"
+#include "arpa/inet.h"
+
+struct icmp* icmp_header_decode(struct subuff *sub){ 
+  	struct icmp* _proto_header = sub->head + ETH_HEADER_LEN + IP_HEADER_LEN;
+	_proto_header->checksum = ntohs(_proto_header->checksum);
+	return _proto_header;
+}
+
+void icmp_rx(struct subuff *sub)
+{
+	struct icmp* icmp_packet = icmp_header_decode(sub);
+	int csum = -1;
+	
+	switch(icmp_packet->type) {
+		case ICMP_V4_REPLY:
+			printf("[#] Found Possible ICMP_REPLY Packet\n");
+			goto drop_pkt;
+		case ICMP_V4_ECHO:
+			printf("[#] Found Possible ICMP_ECHO Packet\n");
+			goto drop_pkt;
+	} 
+    //FIXME: implement your ICMP packet processing implementation here
+    //figure out various type of ICMP packets, and implement the ECHO response type (icmp_reply)
+	drop_pkt:
+	free_sub(sub);
+}
+
+void icmp_reply(struct subuff *sub)
+{
+    //FIXME: implement your ICMP reply implementation here
+    // preapre an ICMP response buffer
+    // send it out on ip_ouput(...)
+}
