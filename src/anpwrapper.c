f5190e7f9275b48678af280b27127f31e8cba92c
David Gall <d.c.c.m.gall@student.vu.nl>
2020-10-13 05:54:40 +0200

    debug

--- src/anpwrapper.c
+++ src/anpwrapper.c
@@ -281,7 +281,7 @@ int tcp_rx(struct subuff *sub){
                 reply_hdr->ack=1;
                 reply_hdr->seq = tcp_header->ack_seq; // Increment Seq
                 stream_data->last_seq_sent = ntohl(tcp_header->ack_seq);
-                reply_hdr->ack_seq = htonl(ntohl(tcp_header->seq)+packet_payload_size+1);
+                reply_hdr->ack_seq = htonl(ntohl(tcp_header->seq)+4096);//packet_payload_size+1);
                 stream_data->last_ack_sent = ntohl(reply_hdr->ack_seq);
                 reply_hdr->option_type = 1;
                 reply_hdr->option_length=1;
