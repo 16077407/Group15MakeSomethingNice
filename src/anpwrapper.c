3f104d35009a21164d1e4d9726e1cdbc7188f441
David Gall <d.c.c.m.gall@student.vu.nl>
2020-10-13 04:27:34 +0200

    debug

--- src/anpwrapper.c
+++ src/anpwrapper.c
@@ -260,12 +260,13 @@ int tcp_rx(struct subuff *sub){
                 if (VERBOSE) printf("[@] Last Sent: %ul, recieved ACK: %ul\n", stream_data->last_seq_sent, stream_data->last_seq_acked);
             }
             // Both read/process ACK, but also accept data if available 
-            if (tcp_header->psh && ip_header->len-(IP_HDR_LEN-TCP_HDR_LEN-4)) {
+            if (tcp_header->psh || ip_header->len-(IP_HDR_LEN-TCP_HDR_LEN-4)) {
                 void *packet_payload = sub->head+ETH_HDR_LEN+IP_HDR_LEN+TCP_HDR_LEN;
                 int packet_payload_size = ip_header->len-IP_HDR_LEN-TCP_HDR_LEN+4;
                 stream_data->bytes_rx+=packet_payload_size;
                 sub_queue_tail(stream_data->rx_in, sub);
 
+                if (tcp_header->psh!=1) return 1;
                 printf("[!!!!!!!!]\n\n");
                 struct subuff* ack = tcp_base(stream_data, ip_header->saddr, ntohs(tcp_header->srcport));
                 struct tcphdr *reply_hdr = (struct tcphdr *)ack->data;
