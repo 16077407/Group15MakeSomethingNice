2d79046873bb4c25cd4ea608d9c2eeccab1fbd5d
David Gall <d.c.c.m.gall@student.vu.nl>
2020-10-13 06:08:10 +0200

    debug

--- src/anpwrapper.c
+++ src/anpwrapper.c
@@ -385,7 +385,7 @@ ssize_t recv (int sockfd, void *buf, size_t len, int flags){
         int read_out = 0;
 
         struct subuff *current = sub_peek(stream_data->rx_in); // Check next payload
-        int current_size = (((struct iphdr *)(current->head+ETH_HDR_LEN))->len)-(IP_HDR_LEN-TCP_HDR_LEN+4); // get size
+        int current_size = (((struct iphdr *)(current->head+ETH_HDR_LEN))->len)-(IP_HDR_LEN+TCP_HDR_LEN+4); // get size
         void *current_start = current->head+ETH_HDR_LEN+IP_HDR_LEN+TCP_HDR_LEN-4; // get start of data
         while(read_out+current_size<=len) { // Check if less than maximum requested size 
             printf("[!!] Size after copy %d\n", current_size+read_out);
@@ -396,7 +396,7 @@ ssize_t recv (int sockfd, void *buf, size_t len, int flags){
             current = sub_peek(stream_data->rx_in); // get next packet
             if (current==NULL) break; // if no next packet, end
             current_start = current->head+ETH_HDR_LEN+IP_HDR_LEN+TCP_HDR_LEN-4; // get new packets payload start
-            current_size = (((struct iphdr *)(current->head+ETH_HDR_LEN))->len)-(IP_HDR_LEN-TCP_HDR_LEN+4); // get size
+            current_size = (((struct iphdr *)(current->head+ETH_HDR_LEN))->len)-(IP_HDR_LEN+TCP_HDR_LEN+4); // get size
         }
         sleep(1);
         return read_out; 
